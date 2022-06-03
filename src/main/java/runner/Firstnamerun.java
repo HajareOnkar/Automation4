f896226ba482fb4378a03c5bd145519a37da5e4b
Onkar <onkarhajare96@gmail.com>
2022-06-03 11:26:56 +0530

    pom+testng+propertyfile

--- src/main/java/runner/Firstnamerun.java
+++ src/main/java/runner/Firstnamerun.java
@@ -7,7 +7,7 @@ import org.testng.annotations.BeforeSuite;
 
 @CucumberOptions (features = {"src/test/resources/Features"}
         ,glue={"stepDefination"},plugin = {"json:target/cucumber.json"})
-public class Firstnamerun extends AbstractTestNGCucumberTestsz
+public class Firstnamerun extends AbstractTestNGCucumberTests
 {
     @BeforeSuite
     public void beforesuit()
